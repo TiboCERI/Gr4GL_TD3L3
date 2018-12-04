bb0b8e45cb0d44ccf9b0a21e11824163aad5b98c
TiboCERI <45357095+TiboCERI@users.noreply.github.com>
2018-12-04 17:15:43 +0100

    final version

--- gl.py
+++ gl.py
@@ -4,54 +4,34 @@ import sys      #argument
 import os       #commande de base
 import os.path  #le path du program
 import shutil   #pour suprimmer récursivement
-import subprocess as sp #commande 
-import re       # RegEx
 
 def filtre(src,dst,element):
-		dst.write("Nom du pdf : "+element.replace('.txt','').replace('_',' ')+"\n")
-		titre = element.replace('.txt','').replace('_',' ')
-		titre.split("20"*)
-		txt = src.read()
-		
-		"""titre = ""
-		for ligne in txt :
-			if ligne == '\n' :
-				break
-			else:
-				titre += ligne
-		dst.write("Titre du pdf : "+titre+"\n")
-		"""
-		if "ABSTRACT" in txt :
-			a0 = txt.split("ABSTRACT",1)
-			
-		
+		titre = element.replace('.txt','').replace('_',' ') 		# on remplace les underscore par des espaces et on enleve l'extension du fichier 
+		dst.write("Nom du pdf : "+titre+"\n")				 		# on ecrit le nom du pdf dans le fichier de destination (premiere ligne)					
+		for i in range(2000,2019) :								
+			if str(i) in titre :
+				annee = str(i)	
+		dst.write("Titre : "+  titre.split(annee)[1] + "\n")  		# on ecrit le titre du pdf dans le fichier de destination (deuxieme ligne)
+		txt = src.read()											# lecture du fichier dans une variable string
+		if "ABSTRACT" in txt :										# blocs de conditions pour identifier le début du résumé  
+			debut = txt.split("ABSTRACT",1)						
+								
 		elif "Abstract" in txt :
-			a0 = txt.split("Abstract",1)
+			debut = txt.split("Abstract",1)
 		
-		k="1"
-		
-		if "Keywords" in txt :
-			k="Keywords"
+		fin="1"
+		if "Keywords" in txt :										# blocs de conditions pour identifier la fin du résumé
+			fin="Keywords"
 			
 		elif "Index" in txt :
-			k="Index"
+			fin="Index"
 		
-		a1 = a0[1].split(k)
+		a1 = debut[1].split(fin)
 		a2 = a1[0].split("\n")
 		dst.write("Resumé : ")
-		for i in range(0,len(a2)) :
+		for i in range(0,len(a2)) :									# ecriture du résumé dans le fichier de destination sur une seule ligne (troisieme ligne)
 			dst.write(a2[i])
 		
-		
-				
-		
-		
-			
-			
-		
-		
-		
-	
 
 
 def transmog(arg):
@@ -61,15 +41,15 @@ def transmog(arg):
     os.mkdir(tmp)
     t = "{}/tmp".format(arg)
     for element in os.listdir(t):
-        if element.endswith('.txt'):
-			a = "{0}/result/".format(arg)
-			s = "{0}/tmp/".format(arg)
-			source = open(s+element,"r")
-			destination = open(a+element, "w")	
-			filtre(source,destination,element)
-			source.close()
-			destination.close()
-	#shutil.rmtree(t)
+	    if element.endswith('.txt'):
+		    a = "{0}/result/".format(arg)
+		    s = "{0}/tmp/".format(arg)
+		    source = open(s+element,"r")
+		    destination = open(a+element, "w")	
+		    filtre(source,destination,element)
+		    source.close()
+		    destination.close()
+
 
 def pdf(arg):
     tmp = "{}/tmp".format(arg)
@@ -95,8 +75,10 @@ if len(sys.argv) != 2:
 else:
     current = os.getcwd()
     if os.path.exists(sys.argv[1]) & os.path.isdir(sys.argv[1]):
-        pdf(sys.argv[1])
+        pdf(sys.argv[1])	
         transmog(sys.argv[1])
+        t = "{}/tmp".format(sys.argv[1])
+        shutil.rmtree(t)
     else:
         print( "L'argument n'éxiste pas ou n'est pas un répertoire !")
         sys.exit(2)
